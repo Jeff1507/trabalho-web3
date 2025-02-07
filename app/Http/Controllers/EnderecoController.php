01841943977471bec2254e5d304673d2d16c9085
JEFF1507 <jeffchaves1507@gmail.com>
2025-02-07 17:53:49 -0300

    aaaa

--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\EnderecoRepository;
+use App\Repositories\ClienteRepository;
 use App\Models\Endereco;
 
 
@@ -19,7 +20,8 @@ class EnderecoController extends Controller
      */
     public function index()
     {
-        //
+        $data = $this->repository->selectAllWith(['cliente']);
+        return $data;
     }
 
     /**
@@ -35,7 +37,21 @@ class EnderecoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $objCliente = (new ClienteRepository())->findById($request->cliente_id);
+        if(isset($objCliente)) {
+            $obj = new Endereco();
+            $obj->cep = $request->cep;
+            $obj->rua = mb_strtoupper($request->rua, 'UTF-8');
+            $obj->bairro = mb_strtoupper($request->bairro, 'UTF-8');
+            $obj->cidade = mb_strtoupper($request->cidade, 'UTF-8');
+            $obj->estado = mb_strtoupper($request->estado, 'UTF-8');
+            $obj->numero = $request->numero;
+            $obj->complemento = mb_strtoupper($request->complemento, 'UTF-8');
+            $obj->cliente()->associate($objCliente);
+            $this->repository->save($obj);
+            return "<h1>Store - OK!</h1>";
+        }
+        return "<h1>Store - Endereco não encontrado!</h1>";
     }
 
     /**
@@ -43,7 +59,8 @@ class EnderecoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data = $this->repository->findById($id);
+        return $data;
     }
 
     /**
@@ -59,7 +76,20 @@ class EnderecoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $obj = $this->repository->findById($id);
+        $objCliente = (new ClienteRepository())->findById($request->cliente_id);
+        if (isset($obj) && isset($objCliente)) {
+            $obj->cep = $request->cep;
+            $obj->rua = mb_strtoupper($request->rua, 'UTF-8');
+            $obj->bairro = mb_strtoupper($request->bairro, 'UTF-8');
+            $obj->cidade = mb_strtoupper($request->cidade, 'UTF-8');
+            $obj->estado = mb_strtoupper($request->estado, 'UTF-8');
+            $obj->numero = $request->numero;
+            $obj->complemento = mb_strtoupper($request->complemento, 'UTF-8');
+            $obj->cliente()->associate($objCliente);
+            $this->repository->save($obj);
+            return "<h1>Update - OK!</h1>";
+        }
     }
 
     /**
@@ -67,6 +97,9 @@ class EnderecoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        if($this->repository->delete($id)) {
+            return "<h1>Delete - OK!</h1>";
+        }
+        return "<h1>Delete - Endereco não encontrado!</h1>";
     }
 }
